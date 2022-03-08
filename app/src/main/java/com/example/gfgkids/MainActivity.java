3a27ac2dffe70b02eb2c84366ea4d1fed3e8aff5
JaviFRx <JAVIERFERNANDEZRAMOS@GMAIL.COM>
2022-03-08 13:17:51 +0100

    anuncios

--- app/src/main/java/com/example/gfgkids/MainActivity.java
+++ app/src/main/java/com/example/gfgkids/MainActivity.java
@@ -1,17 +1,43 @@
 package com.example.gfgkids;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdError;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private InterstitialAd mInterstitialAd;
+    private final String TAG = "myApp";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {}
+        });
     }
 
     public void pantalla_wedo(View view){
@@ -22,8 +48,7 @@ public class MainActivity extends AppCompatActivity {
         Intent pantalla_new = new Intent(this, ev3.class);
         startActivity(pantalla_new);
     }
-    public  void ir_inicio(View view) {
-        Intent pantalla_new = new Intent(this, MainActivity.class);
-        startActivity(pantalla_new);
-    }
+    @Override
+    public void onBackPressed(){    }
+
 }
--- app/src/main/java/com/example/gfgkids/pdf.java
+++ app/src/main/java/com/example/gfgkids/pdf.java
@@ -1,16 +1,28 @@
 package com.example.gfgkids;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.github.barteksc.pdfviewer.PDFView;
 import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle;
 import com.github.barteksc.pdfviewer.util.FitPolicy;
+import com.google.android.gms.ads.AdError;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
+
 
 public class pdf extends AppCompatActivity {
+
+    private InterstitialAd mInterstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +54,61 @@ public class pdf extends AppCompatActivity {
     }
 
     public  void ir_inicio(View view){
+        publicidad();
+       // volver_al_inicio();
+    }
+    public void volver_al_inicio(){
         Intent pantalla_new = new Intent(this, MainActivity.class);
         startActivity(pantalla_new);
     }
+
+    public void publicidad( ){
+        AdRequest adRequest = new AdRequest.Builder().build();
+        InterstitialAd.load(pdf.this,"ca-app-pub-3940256099942544/1033173712", adRequest,
+                new InterstitialAdLoadCallback() {
+                    @Override
+                    public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                        mInterstitialAd = interstitialAd;
+                        Log.i("TAG", "onAdLoaded");
+
+                        mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback(){
+                            @Override
+                            public void onAdDismissedFullScreenContent() {
+                                Log.d("TAG", "The ad was dismissed.");
+                                volver_al_inicio();
+                            }
+
+
+                            @Override
+                            public void onAdFailedToShowFullScreenContent( @NonNull AdError adError) {
+                                Log.d("TAG", "The ad failed to show.");
+                            }
+
+                            @Override
+                            public void onAdShowedFullScreenContent() {
+                                mInterstitialAd = null;
+                                Log.d("TAG", "The ad was shown.");
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                        // Handle the error
+                        Log.i("TAG", loadAdError.getMessage());
+                        mInterstitialAd = null;
+                    }
+                });
+        if (mInterstitialAd != null) {
+            mInterstitialAd.show(pdf.this);
+        } else {
+            Log.d("TAG", "The interstitial ad wasn't ready yet.");
+        }
+    }
+
+    @Override
+    public void onBackPressed(){
+        publicidad();
+    }
+
 }
